403d004fb95dd31c295cfb486a298151cdf27b88
wahaj4311 <wahaj4311@gmail.com>
2025-03-28 13:47:34 +0500

    Add service principal authentication for Terraform

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -50,4 +50,25 @@ variable "db_password" {
   description = "Database password"
   type        = string
   sensitive   = true
+}
+
+variable "client_id" {
+  description = "Azure Service Principal client ID"
+  type        = string
+}
+
+variable "client_secret" {
+  description = "Azure Service Principal client secret"
+  type        = string
+  sensitive   = true
+}
+
+variable "tenant_id" {
+  description = "Azure tenant ID"
+  type        = string
+}
+
+variable "subscription_id" {
+  description = "Azure subscription ID"
+  type        = string
 } 
